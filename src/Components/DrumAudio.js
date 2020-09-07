f59c9e6dde12dee1ecc604faa4c0a39518948501
unknown <quiramapino347@gmail.com>
2020-09-06 19:44:57 -0500

    actualizaciones

---
+++ src/Components/DrumAudio.js
@@ -0,0 +1,24 @@
+const drumAudio = [
+  {
+    keyCode: 81,
+    keyTrigger: "Q",
+    id: "Q",
+    url: "http://www.waveevents.com/MyFilez/wavs/variety/scream2.wav",
+  },
+  {
+    id: "C",
+    keyboard: "Screen",
+    url: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3",
+  },
+  {
+    id: "M",
+    keyboard: "Screen",
+    url: "http://www.waveevents.com/MyFilez/wavs/variety/scream2.wav",
+  },
+  {
+    id: "A",
+    keyboard: "Screen",
+    url: "http://www.waveevents.com/MyFilez/wavs/variety/scream2.wav",
+  },
+];
+export default drumAudio;
